1bb089271d90a25245ee3c348ef1b0faafd32be8
surbhi <surbhi337865@gmail.com>
2022-06-22 22:42:19 +0530

    reusable card componet

---
+++ src/app/components/HomePage/Card.tsx
@@ -0,0 +1,55 @@
+import { Box, Image } from '@chakra-ui/react';
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+export function Card({
+  imageSrc,
+  imageAlt,
+  imgTitle,
+  imageName,
+  imgDate,
+  imgTime,
+}) {
+  return (
+    <Wrapper>
+      {/* <Image src={imageSrc} alt={imageAlt} /> */}
+      <Box borderWidth="1px" borderRadius="lg" h="300px" mt="24px">
+        <Image src={imageSrc} alt={imageAlt} w="100%" h="190px" />
+        <Box p="4">
+          <Box
+            // mt="1"
+            fontWeight="bold"
+            as="h4"
+            // lineHeight="tight"
+            // noOfLines={1}
+          >
+            {imgTitle}
+          </Box>
+
+          <Box
+            display="flex"
+            mt="2"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Box>{imageName}</Box>
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {imgDate} {imgTime}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.nav`
+  /* display: flex;
+  justify-content: space-between; */
+  /*font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 2px; */
+  /* margin-top: 50px; */
+  /* background-color: aliceblue; */
+  /* width: 41.5em; */
+`;
---
+++ src/app/pages/HomePage/HomeNav.tsx
@@ -0,0 +1,57 @@
+import { Box } from '@chakra-ui/react';
+import { Logo } from 'app/components/NavBar/Logo';
+import { Menu } from 'app/components/NavBar/Menu';
+import { Search } from 'app/components/NavBar/Search';
+import { PageWrapper } from 'app/components/PageWrapper';
+import * as React from 'react';
+import styled from 'styled-components/macro';
+import { StyleConstants } from 'styles/StyleConstants';
+import { TimeIcon, SettingsIcon } from '@chakra-ui/icons';
+import { MenuIcon } from 'app/components/NavBar/MenuIcon';
+
+export default function HomeNav() {
+  return (
+    <>
+      <Wrapper style={{ backgroundColor: '#183380', color: 'white' }}>
+        <PageWrapper
+          style={{
+            display: 'flex',
+            paddingTop: '11px',
+            paddingBottom: '11px',
+            width: '100%',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box d="flex">
+            <Logo />
+            <Search />
+          </Box>
+          <Box d="flex">
+            <Menu iconUrl={<TimeIcon />} Icontext="Explore" />
+            <Menu iconUrl={<TimeIcon />} Icontext="Bookmarks" />
+            <Menu iconUrl={<TimeIcon />} Icontext="Start writing" />
+            <MenuIcon iconUrl={<SettingsIcon />} />
+            <MenuIcon iconUrl={<SettingsIcon />} />
+            <MenuIcon iconUrl={<SettingsIcon />} />
+          </Box>
+        </PageWrapper>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.header`
+  box-shadow: 0 1px 0 0 ${p => p.theme.borderLight};
+  height: ${StyleConstants.NAV_BAR_HEIGHT};
+  display: flex;
+  /* position: fixed; */
+  top: 0;
+  width: 100%;
+  font-family: 'Nunito Sans', NunitoSans, Arial, sans-serif;
+  font-size: 16px;
+
+  ${PageWrapper} {
+    display: flex;
+    align-items: center;
+  }
+`;
