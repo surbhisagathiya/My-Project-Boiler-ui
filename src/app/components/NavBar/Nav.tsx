089a0a82dc265cba4d26a6d354037a109aacca43
surbhi <surbhi337865@gmail.com>
2022-06-23 14:29:14 +0530

    convert in typescript navbar

--- src/app/components/NavBar/Nav.tsx
+++ src/app/components/NavBar/Nav.tsx
@@ -2,20 +2,24 @@ import { Text } from '@chakra-ui/react';
 import * as React from 'react';
 import styled from 'styled-components/macro';
 
-export function Nav(props) {
-  return (
-    <Wrapper
-      style={{
-        position: 'fixed',
-        width: '100%',
-        textAlign: 'center',
-      }}
-    >
-      <Text>{props.title}</Text>
-    </Wrapper>
-  );
+interface navProps {
+  title: string;
 }
+type Props = navProps;
 
+const Nav: React.FC<Props> = props => (
+  <Wrapper
+    style={{
+      position: 'fixed',
+      width: '100%',
+      textAlign: 'center',
+    }}
+  >
+    <Text>{props.title}</Text>
+  </Wrapper>
+);
+
+export default Nav;
 const Wrapper = styled.nav`
   display: flex;
   justify-content: center;
