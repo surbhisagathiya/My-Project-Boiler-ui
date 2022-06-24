c5917b555aa84fb35fb3ea57203834b145a018ee
surbhi <surbhi337865@gmail.com>
2022-06-24 16:07:05 +0530

    slice generate

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,3 +1,4 @@
+import { LoginState } from 'app/pages/HomePage/slice/types';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 /* 
@@ -5,5 +6,6 @@
   You have to declare them here manually
 */
 export interface RootState {
+  login?: LoginState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
