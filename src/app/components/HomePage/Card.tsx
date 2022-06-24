c5f6f39ad60eb55c6c1e8f86ce5c3ff685e5e54c
surbhi <surbhi337865@gmail.com>
2022-06-24 11:18:29 +0530

    adding overlay on image

--- src/app/components/HomePage/Card.tsx
+++ src/app/components/HomePage/Card.tsx
@@ -1,5 +1,8 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Badge, Box, Image } from '@chakra-ui/react';
+import { faBookmark, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
+// import { Link } from 'react-router-dom';
 
 interface cardProps {
   imageSrc: string;
@@ -15,7 +18,32 @@ type Props = cardProps;
 const Card: React.FC<Props> = props => (
   <>
     <Box borderWidth="1px" borderRadius="lg" h="300px" mt="24px">
-      <Image src={props.imageSrc} alt={props.imageAlt} w="100%" h="190px" />
+      <Box position="relative">
+        <Image src={props.imageSrc} alt={props.imageAlt} w="100%" h="190px" />
+        <FontAwesomeIcon
+          icon={faBookmark}
+          color="white"
+          style={{ position: 'absolute', top: 15, right: 50 }}
+          size="lg"
+        />
+        <FontAwesomeIcon
+          icon={faEllipsisV}
+          color="white"
+          style={{ position: 'absolute', top: 15, right: 20 }}
+          size="lg"
+        />
+        <Badge
+          variant="outline"
+          color="white"
+          style={{ position: 'absolute', bottom: 10, right: 10 }}
+          py="5px"
+          px="12px"
+          borderRadius="12px"
+          border="1px solid rgba(255, 255, 255, 0.5);"
+        >
+          $ 0.45
+        </Badge>
+      </Box>
       <Box p="0.8em" backgroundColor="white">
         <Box fontWeight="bold" as="h4">
           {props.imgTitle}
